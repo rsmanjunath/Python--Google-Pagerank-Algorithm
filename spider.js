de3a8444acbcbdbfea97a6cf0bebd0291688668a
Manjunath <rsmanjunath97@gmail.com>
2019-07-03 13:14:55 +0530

    Add files via upload

---
+++ spider.js
@@ -0,0 +1,8 @@
+spiderJson = {"nodes":[
+{"weight":1,"rank":0.0, "id":1, "url":"http://python-data.dr-chuck.net"},
+{"weight":1,"rank":9.499999999999996, "id":7, "url":"http://python-data.dr-chuck.net/known_by_42.html"},
+{"weight":1,"rank":19.0, "id":80, "url":"http://python-data.dr-chuck.net/known_by_Cullin.html"}],
+"links":[
+{"source":0,"target":1,"value":3},
+{"source":0,"target":0,"value":3},
+{"source":1,"target":2,"value":3}]};
